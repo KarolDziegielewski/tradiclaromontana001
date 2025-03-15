e90a31c014b8d3a4845d200f258d9b64ebaba08d
kdziegie <01197564@pw.edu.pl>
2025-03-15 21:19:26 +0100

    igyk

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/registration_screen.dart';
+import 'texts/keys.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: "AIzaSyBPGzueFzJJf0OCPITeeiEWjHaLIM-38JE",
-      authDomain: "tradiclaromontana.firebaseapp.com",
-      projectId: "tradiclaromontana",
-      storageBucket: "tradiclaromontana.firebasestorage.app",
-      messagingSenderId: "242551174305",
-      appId: "1:242551174305:web:f9c226e77139186498ece0",
+      apiKey: apiKey_mine,
+      authDomain: authDomain_mine,
+      projectId: projectId_mine,
+      storageBucket: storageBucket_mine,
+      messagingSenderId: messagingSenderId_mine,
+      appId: appId_mine,
     ),
   );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
