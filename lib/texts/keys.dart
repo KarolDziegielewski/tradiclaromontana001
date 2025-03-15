cfd822481e4dd5a2d79b9643d04e5ca6ad521ea9
kdziegie <01197564@pw.edu.pl>
2025-03-15 21:25:10 +0100

    2u5q

---
+++ lib/texts/keys.dart
@@ -0,0 +1,6 @@
+      String apiKey_mine = "AIzaSyBPGzueFzJJf0OCPITeeiEWjHaLIM-38JE";
+      String authDomain_mine  = "tradiclaromontana.firebaseapp.com";
+      String projectId_mine = "tradiclaromontana";
+      String storageBucket_mine = "tradiclaromontana.firebasestorage.app";
+      String messagingSenderId_mine = "242551174305";
+      String appId_mine = "1:242551174305:web:f9c226e77139186498ece0";
